e799aa261eebcd49e01799825bdbad922aa9b0f0
Tạ Quang Khôi <45689286+TaQuangKhoi@users.noreply.github.com>
2021-08-12 11:00:44 +0700

    Chức Năng Cập Nhật Phân Số + minor update
    Chỉnh lại các Method so sánh..

--- ChucNang.cs
+++ ChucNang.cs
@@ -44,6 +44,47 @@ namespace TinhToanPhanSo
                 }
             }while(true);
         }
+        public static void UpdatePS(ArrayList arrPS)
+        {
+            Console.Clear();
+            Console.WriteLine("1. Cập Nhật Phân Số 1");
+            Console.WriteLine("2. Cập Nhật Phân Số 2");
+            Console.WriteLine("0. Màn Hình Chính");
+            int iChucNang = int.Parse(Console.ReadLine());
+            do
+            {
+                switch (iChucNang)
+                {
+                    case 0: // Về Menu Chính
+                        {
+                            Program.Programming(arrPS);
+                            break;
+                        }
+                    case 1: // Nhập Phân SỐ 1
+                        {
+                            PhanSo phanso = new PhanSo();
+                            phanso = (PhanSo)arrPS[0];
+                            phanso.XuatPhanSo();
+                            PhanSo phanso1 = new PhanSo();
+                            arrPS.Insert(0, phanso1);
+                            phanso1.NhapPhanSo();
+                            UpdatePS(arrPS);
+                            break;
+                        }
+                    case 2: //Nhập Phần Số 2
+                        {
+                            PhanSo phanso = new PhanSo();
+                            phanso = (PhanSo)arrPS[1];
+                            phanso.XuatPhanSo();
+                            PhanSo phanso2 = new PhanSo();
+                            arrPS.Insert(1, phanso2);
+                            phanso2.NhapPhanSo();
+                            UpdatePS(arrPS);
+                            break;
+                        }
+                }
+            } while (true);
+        }
         public static void HienPhanSo(ArrayList arrPS)
         {
             Console.Clear();
@@ -144,9 +185,84 @@ namespace TinhToanPhanSo
                         }
                     case 1://Bằng ==
                         {
+                            PhanSo ps1 = (PhanSo)arrPS[0];
+                            PhanSo ps2 = (PhanSo)arrPS[1];
+                            if (ps1 == ps2)
+                            {
+                                Console.WriteLine("Phân số 1 = Phân số 2");
+                                Console.ReadLine();
+                                ChucNang.SoSanh(arrPS);
+                            }
+                            else
+                                Console.WriteLine("Phân số 1 không = Phân số 2");
+                            break;
+                        }
+                    case 2: //2. Lớn Hơn >
+                        {
+                            PhanSo ps1 = (PhanSo)arrPS[0];
+                            PhanSo ps2 = (PhanSo)arrPS[1];
+                            if (ps1 > ps2)
+                            {
+                                Console.WriteLine("Phân số 1 > Phân số 2");
+                                Console.ReadLine();
+                                ChucNang.SoSanh(arrPS);
+                            }
+                            else
+                                Console.WriteLine("Phân số 1 không > Phân số 2");
+                                Console.ReadLine();
+                                ChucNang.SoSanh(arrPS);
+                            break;
+                        }
+                    case 3: // Nhỏ Hơn <
+                        {
+                            PhanSo ps1 = (PhanSo)arrPS[0];
+                            PhanSo ps2 = (PhanSo)arrPS[1];
+                            if (ps1 < ps2)
+                            {
+                                Console.WriteLine("Phân số 1 < Phân số 2");
+                                Console.ReadLine();
+                                ChucNang.SoSanh(arrPS);
+                            }
+                            else
+                                Console.WriteLine("Phân số 1 không < Phân số 2");
+                                Console.ReadLine();
+                                ChucNang.SoSanh(arrPS);
+                            break;
+                        }
+                    case 4:// Lớn Hơn or Bằng >=
+                        {
+                            PhanSo ps1 = (PhanSo)arrPS[0];
+                            PhanSo ps2 = (PhanSo)arrPS[1];
+                            if (ps1 >= ps2)
+                            {
+                                Console.WriteLine("Phân số 1 >= Phân số 2");
+                                Console.ReadLine();
+                                ChucNang.SoSanh(arrPS);
+                            }
+                            else
+                                Console.WriteLine("Phân số 1 không >= Phân số 2");
+                            Console.ReadLine();
+                            ChucNang.SoSanh(arrPS);
+                            break;
+                        }
+                    case 5:// Nhỏ Hơn or Bằng <=
+                        {
+                            PhanSo ps1 = (PhanSo)arrPS[0];
+                            PhanSo ps2 = (PhanSo)arrPS[1];
+                            if (ps1 <= ps2)
+                            {
+                                Console.WriteLine("Phân số 1 <= Phân số 2");
+                                Console.ReadLine();
+                                ChucNang.SoSanh(arrPS);
+                            }
+                            else
+                                Console.WriteLine("Phân số 1 không <= Phân số 2");
+                                Console.ReadLine();
+                                ChucNang.SoSanh(arrPS);
                             break;
                         }
                 }
             } while (true);
+        }
     }
 }
